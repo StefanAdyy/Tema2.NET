77baef9ac48fcb927fa69ece9a54dc7ac293644b
StefanAdyy <stefanmorariu@rocketmail.com>
2023-04-25 22:46:51 +0300

    Added UserLoginDto

---
+++ DataLayer/Dtos/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Dtos
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
